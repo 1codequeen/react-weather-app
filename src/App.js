972a9b36d3a3dfe92814823c1cdd412c157a651d
1codequeen <queenyona21@gmail.com>
2026-02-14 15:33:16 +0200

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Queen Yona and is open-sourced on{" "}
         <a href="https://github.com/1codequeen/react-weather-app" target="_blank" rel="noreferrer">
         Github</a>
       </footer>
+</div>
     </div>
   );
 }
