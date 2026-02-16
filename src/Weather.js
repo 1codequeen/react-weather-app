2b4a342257952b853f1db70f3914fbcceadf1966
1codequeen <queenyona21@gmail.com>
2026-02-16 18:01:54 +0200

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
-
 export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.temperature.current);
+    setReady(true);
+  }
+if (ready) {
   return (
     <div className="Weather">
-        <form>
-            <div className="row">
-         <div className="col-9">
-        <input type="search" placeholder="Enter a city..." className="form-control"/>
-       </div>
-         <div className="col-3">
-        <input type="submit" value="Search" />
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
-        </div>
-        </form>
+      </form>
       <h1>Lisbon</h1>
       <ul>
-        <li> Monday 10:00</li>
-        <li> Mostly Cloudy</li>
+        <li> Monday 10:00</li> <li> Mostly Cloudy</li>
       </ul>
 
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
-            alt="Mostly Cloudy"
-          />
+          <div className="weather-temperature">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="Mostly Cloudy"
+            />
+          </div>
+          <div className="temperature-container">
+            <span className="temperature">{temperature}</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
-        6°C
-        <div className="col-6">
+        <div className="col-4 offset-2">
           <ul>
-            <li>Precipitation: 10%</li>
-            <li>Humidity: 70%</li>
+            <li>Precipitation: 10%</li> <li>Humidity: 70%</li>
             <li>Wind: 12 km/h</li>
           </ul>
         </div>
       </div>
     </div>
   );
+}else {
+const apiKey = "bf54175800a55e59e6c4d6461deeef12";
+let city = "Lisbon";
+const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading...";
+}
 }
