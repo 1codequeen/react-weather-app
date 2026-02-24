27c370a47dffa6029c044f229106a8cf739dc092
1codequeen <queenyona21@gmail.com>
2026-02-24 14:08:49 +0200

    edited icon page

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
- const codeMapping = {
-   "clear-day": "CLEAR_DAY",
-   "clear-night": "CLEAR_NIGHT",
-   "partly-cloudy-day": "PARTLY_CLOUDY_DAY",
-   "partly-cloudy-night": "PARTLY_CLOUDY_NIGHT",
-   cloudy: "CLOUDY",
-   "rain-day": "RAIN",
-   "rain-night": "RAIN",
-   "snow-day": "SNOW",
-   "snow-night": "SNOW",
-   "fog-day": "FOG",
-   "fog-night": "FOG",
- };
- console.log("WeatherIcon received code:", props.code);
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "partly-cloudy-day": "PARTLY_CLOUDY_DAY",
+    "partly-cloudy-night": "PARTLY_CLOUDY_NIGHT",
+    cloudy: "CLOUDY",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "fog-day": "FOG",
+    "fog-night": "FOG",
+  };
+  console.log("WeatherIcon received code:", props.code);
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
